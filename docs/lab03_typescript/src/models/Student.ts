1e8cb74d4d0d03a653d2e1d1dca12961698a7c6a
Ginny421 <duangruethai.a@kkumail.com>
2025-12-10 20:04:01 +0700

    lab3

---
+++ docs/lab03_typescript/src/models/Student.ts
@@ -0,0 +1,9 @@
+export interface Student {
+  id: string;
+  title_name: string;   // เช่น นาย, นางสาว
+  first_name: string;
+  last_name: string;
+  year: number;
+  major: string;
+  email: string;
+}
